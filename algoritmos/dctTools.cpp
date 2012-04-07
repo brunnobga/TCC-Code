018a27b5e3973012550dd14818ffee18a6b42a49
Brunno Braga <brunnobga@gmail.com>
2012-04-07 11:32:07 -0300

    testes funcionando com imagem inteira

--- algoritmos/dctTools.cpp
+++ algoritmos/dctTools.cpp
@@ -2,10 +2,13 @@
 
 #include <cmath>
 #include <cstdlib>
+#include <cstdio>
 
 typedef unsigned char byte;
 
-//Aplica DCT2 em um bloco blockSize X blockSize contido em um frame de largura frameW.
+// Aplica DCT2 em um bloco blockSize X blockSize contido em um frame de largura frameW.
+// blockFrom aponta para o primeiro pixel a ser processado em um frame de largura frameW
+// blockTo deve ter tamanho blockSize*blockSize
 void applyDCT2(byte *blockFrom, float *blockTo, int blockSize, int frameW){
 	float a, b;
 	int p, q, mm, nn;
@@ -17,12 +20,11 @@ void applyDCT2(byte *blockFrom, float *blockTo, int blockSize, int frameW){
 			if(q == 0) b = 1/sqrt(blockSize);
 			else b = sqrt(2/blockSize);
 			//r[p][q] = 0;
-			*(blockTo + p*frameW + q) = 0;
+			*(blockTo + p*blockSize + q) = 0;
 			for(mm = 0; mm < blockSize; mm++){
 				for(nn = 0; nn < blockSize; nn++){
 					//r[p][q] += a*b*o[mm][nn]*cos(M_PI*(2*mm+1)*p/(2*m))*cos(M_PI*(2*nn+1)*q/(2*n));
-					*(blockTo + p*frameW + q) += *(blockFrom + mm*frameW + nn)*a*b*cos(M_PI*(2*mm+1)*p/(2*blockSize))*cos(M_PI*(2*nn+1)*q/(2*blockSize));
-					//*(blockTo + p*frameW + q) += a*b*cos(M_PI*(2*mm+1)*p/(2*blockSize))*cos(M_PI*(2*nn+1)*q/(2*blockSize));
+					*(blockTo + p*blockSize + q) += *(blockFrom + mm*frameW + nn)*a*b*cos(M_PI*(2*mm+1)*p/(2*blockSize))*cos(M_PI*(2*nn+1)*q/(2*blockSize));
 				}
 			}
 		}
@@ -31,4 +33,24 @@ void applyDCT2(byte *blockFrom, float *blockTo, int blockSize, int frameW){
 }
 
 //Aplica IDCT2 em um bloco blockSize X blockSize contido em um frame de largura frameW.
-void applyIDCT2(float* blockFrom, byte* blockTo, int blockSize, int frameW){}
+void applyIDCT2(float* blockFrom, byte* blockTo, int blockSize, int frameW){
+	float a, b;
+	int p, q, mm, nn;
+	
+	for(int mm = 0; mm < blockSize; mm++){
+		for(int nn = 0; nn < blockSize; nn++){
+			//r[mm][nn] = 0;
+			*(blockTo + mm*frameW + nn) = 0;
+			for(int p = 0; p < blockSize; p++){
+				for(int q = 0; q < blockSize; q++){
+					if(p == 0) a = 1/sqrt(blockSize);
+					else a = sqrt(2/blockSize);
+					if(q == 0) b = 1/sqrt(blockSize);
+					else b = sqrt(2/blockSize);
+					//r[mm][nn] += a*b*o[p][q]*cos(M_PI*(2*mm+1)*p/(2*blockSize))*cos(M_PI*(2*nn+1)*q/(2*blockSize));
+					*(blockTo + mm*frameW + nn) += *(blockFrom + p*blockSize + q)*a*b*cos(M_PI*(2*mm+1)*p/(2*blockSize))*cos(M_PI*(2*nn+1)*q/(2*blockSize));
+				}
+			}	
+		}
+	}
+}
--- algoritmos/testes.cpp
+++ algoritmos/testes.cpp
@@ -1,16 +1,59 @@
 #include "dctTools.h"
 #include <cstdio>
+#include <fstream>
+#include <cstdlib>
+
+#define W 352
+#define H 288
+#define BLOCK_SIZE 8 
+
+using namespace std;
 
 int main(){
-	byte a[4][4] = {{1,1,1,1},{1,1,1,1},{1,1,1,1},{1,1,1,1}};
-	float b[4][4];
+	ifstream fileYUV("/mnt/vbox/video/waterfall_352x288_25Hz_P420.yuv", ifstream::binary);
+	//ifstream fileYUV("/mnt/vbox/video/parkrun_1280x720_25Hz_P420.yuv", ifstream::binary);
+	ofstream fileBlock("/mnt/vbox/video/waterblur_352x288_25Hz_P420.yuv", ofstream::binary);
+	//ofstream fileBlock("/mnt/vbox/video/parkblur_1280x720_25Hz_P420.yuv", ofstream::binary);
+	byte *frame, *outframe;
+	int i, j, k = 0;
+	float dct[BLOCK_SIZE][BLOCK_SIZE];
+	
+	//alocar espaço para frames
+	frame = (byte*)malloc(W*H*sizeof(byte));
+	outframe = (byte*)malloc(W*H*sizeof(byte));
 
-	applyDCT2(&a[0][0], &b[0][0], 4, 4);
+	//enqto não chegar ao fim do video
+	while(!fileYUV.eof()){
+	//for(k = 0; k < 10;){
 
-	for(int i = 0; i < 4; i++){
-		for(int j = 0; j < 4; j++){
-			printf("%f ", b[i][j]);
+		//ler 1 frame
+		fileYUV.read((char*)frame, H*W);
+
+		if(fileYUV.eof() || fileYUV.fail()) return 0;
+
+		for(i = 0; i < 288/BLOCK_SIZE; i++){
+			for(j = 0; j < 352/BLOCK_SIZE; j++){
+				//printf("i-> %d j-> %d\n", i, j);
+				applyDCT2(frame + i*W*BLOCK_SIZE + j*BLOCK_SIZE, &dct[0][0], BLOCK_SIZE, W);
+				applyIDCT2(&dct[0][0], outframe + i*W*BLOCK_SIZE + j*BLOCK_SIZE, BLOCK_SIZE, W);
+			}
 		}
-		printf("\n");
+		
+		fileBlock.write((char*)outframe, H*W);
+		
+		//copia chromas
+		fileYUV.read((char*)frame, H*W/2);
+		fileBlock.write((char*)frame, H*W/2);
+
+		//progresso
+		fprintf(stdout, "Output at frame %d...\n", k++);
 	}
+
+	free(frame);
+	free(outframe);
+	
+	//termina arquivos
+	fileYUV.close();
+	fileBlock.close();
+
 }
