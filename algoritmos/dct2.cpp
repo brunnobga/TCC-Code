f60e61ad55ad5ea518ff8f5b603e61522fc2cf39
Caio <caioccaa@gmail.com>
2012-04-07 12:37:12 -0300

    Correção DCT e IDCT

--- algoritmos/dct2.cpp
+++ algoritmos/dct2.cpp
@@ -7,39 +7,41 @@
  */
 #include <cstdio>
 #include <math.h>
+#include <time.h>
 #include <stdlib.h>
 #include <iostream>
 
 using namespace std;
 
 int main(void){
-	int m = 8, n = 8;
-	float a, b, r[m][n], o[m][n];
+	srand(time(NULL));
+	int m, n;
+	scanf("%d%d", &m, &n);
+	double a, b, r[m][n], o[m][n];
 
 	// read input file dct2.in
 	for(int i = 0; i < m; i++){
 		for(int j = 0; j < n; j++){
-			scanf("%f", &o[i][j]);
+			scanf("%lf", &o[i][j]);
 		}
 	}
-
 	for(int p = 0; p < m; p++){
 		for(int q = 0; q < n; q++){
 			if(p == 0) a = 1/sqrt(m);
-			else a = sqrt(2/m);
+			else a = sqrt(2)/sqrt(m);
 			if(q == 0) b = 1/sqrt(n);
-			else b = sqrt(2/n);
+			else b = sqrt(2)/sqrt(n);
 			r[p][q] = 0;
-			for(int mm = 0; mm < m; mm++){
-				for(int nn = 0; nn < n; nn++){
-					r[p][q] += a*b*o[mm][nn]*cos(M_PI*(2*mm+1)*p/(2*m))*cos(M_PI*(2*nn+1)*q/(2*n));
-				}
-			}
+			for(int mm = 0; mm < m; mm++)
+				for(int nn = 0; nn < n; nn++)
+					r[p][q] += o[mm][nn]*cos(M_PI*(2*mm+1)*p/(2*m))*cos(M_PI*(2*nn+1)*q/(2*n));
+			r[p][q] = r[p][q]*a*b;
 		}
 	}
+	printf("%d %d\n", m, n);
 	for(int i = 0; i < m; i++){
+		for(int j = 0; j < n; j++) printf("%3.4lf  ", r[i][j]);
 		printf("\n");
-		for(int j = 0; j < n; j++) printf("%3.4f  ", r[i][j]);
 	}
 	printf("\n");
 
--- algoritmos/dct2i.cpp
+++ algoritmos/dct2i.cpp
@@ -11,13 +11,14 @@
 using namespace std;
 
 int main(void){
-	int m = 8, n = 8;
-	float a, b, r[m][n], o[m][n];
+	int m, n;
+	scanf("%d%d", &m, &n);
+	double a, b, r[m][n], o[m][n];
 
 	// read input file dct2.out
 	for(int i = 0; i < m; i++){
 		for(int j = 0; j < n; j++){
-			scanf("%f", &o[i][j]);
+			scanf("%lf", &o[i][j]);
 		}
 	}
 	for(int mm = 0; mm < m; mm++){
@@ -26,17 +27,18 @@ int main(void){
 			for(int p = 0; p < m; p++){
 				for(int q = 0; q < n; q++){
 					if(p == 0) a = 1/sqrt(m);
-					else a = sqrt(2/m);
+					else a = sqrt(2)/sqrt(m);
 					if(q == 0) b = 1/sqrt(n);
-					else b = sqrt(2/n);
+					else b = sqrt(2)/sqrt(n);
 					r[mm][nn] += a*b*o[p][q]*cos(M_PI*(2*mm+1)*p/(2*m))*cos(M_PI*(2*nn+1)*q/(2*n));
 				}
 			}
 		}
 	}
+	printf("%d %d\n", m, n);
 	for(int i = 0; i < m; i++){
+		for(int j = 0; j < n; j++) printf("%3.4lf  ", r[i][j]);
 		printf("\n");
-		for(int j = 0; j < n; j++) printf("%3.4f  ", r[i][j]);
 	}
 	printf("\n");
 
