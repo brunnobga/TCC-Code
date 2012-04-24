ea43275a4c2299b834936ceb80723f295af30675
Brunno Braga <brunnobga@gmail.com>
2012-04-23 22:34:56 -0300

    metricTool: MSSIM nao funcionando

--- algoritmos/metricTool.cpp
+++ algoritmos/metricTool.cpp
@@ -1,12 +1,19 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
+#include <cmath>
 #include <getopt.h>
 #include <fstream>
 
 #define DEBUG_INPUT
+#define DEBUG_OUTPUT
+
+#define L 255
 
 using namespace std;
+
+static const double c1 = 0.0001*L*L;
+static const double c2 = 0.0009*L*L;
 
 typedef unsigned char byte;
 
@@ -81,6 +88,7 @@ public:
 	}
 
 	int pow2(int a) {return a*a;}
+	double pow2(double a) {return a*a;}
 
 	double mean(double * in, int size){
 		double result = 0;
@@ -101,19 +109,83 @@ public:
 			rateperframe[fc] /= frameSize;
 			input.read((char*)frame, frameSize/2);
 			ref.read((char*)refframe, frameSize/2);
+			#ifdef DEBUG_OUTPUT
+				printf("Frame %d\n", fc);
+			#endif
 		}
 		return mean(rateperframe, frameTotal);
 	}
 
 	double psnr(){
-		return 0.;
+		double m = mse();
+		return 10.*log10(pow2(255)/m);
+	}
+
+	double blockMean(byte * frame, int x, int y){
+		double result = 0;
+		int i, j;
+		for(i = 0; i < window; i++)
+			for(j = 0; j < window; j++)
+				result += *(frame + frameWidth*(x*window + i) + y*window + j);
+		return result / pow2(window);
+	}
+
+	double blockVariance(byte * frame, double m, int x, int y){
+		double result = 0;
+		int i, j;
+		for(i = 0; i < window; i++)
+			for(j = 0; j < window; j++)
+				result += pow2(*(frame + frameWidth*(x*window + i) + y*window + j) - m);
+		return sqrt(result/pow2(window));
+	}
+
+	double blockCovariance(byte * frame, byte * refframe, double m1, double m2, int x, int y){
+		double result = 0;
+		int i, j;
+		for(i = 0; i < window; i++)
+			for(j = 0; j < window; j++)
+				result += (*(frame + frameWidth*(x*window + i) + y*window + j) - m1) * (*(refframe + frameWidth*(x*window + i) + y*window + j) - m2);
+		return result/pow2(window);
+	}
+
+	double ssim(int x, int y){
+		double m1, m2, z1, z2, z12, num, den;
+		m1 = blockMean(frame, x, y);
+		m2 = blockMean(refframe, x, y);
+		z1 = blockVariance(frame, m1, x, y);
+		z2 = blockVariance(refframe, m2, x, y);
+		z12 = blockCovariance(frame, refframe, m1, m2, x, y);
+		num = (2 * m1 * m2 + c1) * (2 * z12 + c2);
+		den = (pow2(m1) + pow2(m2) + c1) * (pow2(z1) + pow2(z2) + c2);
+		return num/den;
+	}
+
+	double mssim(){
+		int i, j, numberW = frameWidth/window, numberH = frameHeight/window;
+		double ssimperframe[numberW][numberH], mssimperframe[frameTotal];
+		for(int fc = 0; fc < frameTotal; fc++){
+			input.read((char*)frame, frameSize);
+			ref.read((char*)refframe, frameSize);
+			for(i = 0; i < numberH; i++){
+				for(j = 0; j < numberW; j++){
+					ssimperframe[i][j] = ssim(i, j);
+				}
+			}
+			input.read((char*)frame, frameSize/2);
+			ref.read((char*)refframe, frameSize/2);
+			mssimperframe[fc] = mean(ssimperframe[0], numberW*numberH);
+			#ifdef DEBUG_OUTPUT
+				printf("Frame %d\n", fc);
+			#endif
+		}
+		return mean(mssimperframe, frameTotal);
 	}
 
 	void performMetric(){
 		double result;
 		if(metricType == 0) result = mse();
 		else if(metricType == 1) result = psnr();
-		else if(metricType == 2);
+		else if(metricType == 2) result = mssim();
 		printf("Result %s: %lf\n", metricstr, result);
 	}
 	
